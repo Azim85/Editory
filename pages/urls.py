d5599aebb2c8574b5ab5a323a83dd4217c5919de
ali <muhammadalixonakbarov2000@gmail.com>
2021-05-11 11:31:38 +0500

    ...

--- pages/urls.py
+++ pages/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('news/', Articles.as_view(), name='news'),
     path('services/', Services.as_view(), name='services'),
     path('paper/<int:pk>/', Paper.as_view(), name='paper'),
+    path('dissertation/', Dissertation.as_view(), name='dissertation'),
+    path('research_strategy/', Research_strategy.as_view(), name='research_strategy'),
 
 ]
