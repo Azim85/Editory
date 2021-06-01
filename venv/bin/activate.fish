c3fb5e87bf7454da35b559c4674308d9560e5a34
beg <asd@asd.asd>
2021-06-01 07:00:41 +0000

    asd

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ali/Documents/Projects/editory/Editory/venv"
+set -gx VIRTUAL_ENV "/var/www/Editory/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/ali/Documents/Projects/editory/Editory/venv/bin/python3
+#!/var/www/Editory/venv/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
