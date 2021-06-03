0d11e184c1d6802ec02996849436356990234709
ali <muhammadalixonakbarov2000@gmail.com>
2021-06-03 12:31:31 +0500

    asd

--- requests/templatetags/tags.py
+++ requests/templatetags/tags.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import Group
 
 register = template.Library()
 
-# @register.filter(name='has_group')
-# def has_group(user, group_name):
-#     group = Group.objects.get(name=group_name)
-#     return True if group in user.groups.all() else False
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    group = Group.objects.get(name=group_name)
+    return True if group in user.groups.all() else False
