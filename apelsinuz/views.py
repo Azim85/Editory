49c1fe99d4e099d5650478d23097b06f05359d3e
Azim85 <avaz_53@icloud.com>
2021-06-21 11:09:39 +0500

    .....

--- apelsinuz/views.py
+++ apelsinuz/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from orders.models import OrderModel
 
 @csrf_exempt
 def check_apelsin(request):
-    data = {"status":"true"}
+
+    data = {"status":"false"}
+    if request.POST['transactionId']:
+        transaction_id = request.POST['transactionId']
+        order = OrderModel.objects.get(id=transaction_id)
+        order.payment_status = 3
+        order.save()
+        order.refresh_from_dbPOST
+        data = {"status":"true"}
     return JsonResponse(data)
