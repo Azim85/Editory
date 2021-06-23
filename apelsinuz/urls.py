7710ecd0c6e6142435ecf573b104be2e6e1e6ca8
Azim85 <avaz_53@icloud.com>
2021-06-23 11:30:27 +0500

    .....

--- apelsinuz/urls.py
+++ apelsinuz/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import check_apelsin
+from .views import check_apelsin, confirm_apelsin
 
 app_name = 'apelsinuz'
 
 urlpatterns = [
-    path('', check_apelsin, name='check-apelsin')
+    path('', check_apelsin, name='check-apelsin'),
+    path('confirm/<int:pk>/', confirm_apelsin, name='confirm')
 ]
