2b37b3c687067abcaff3259c892215adae1e16a0
Azim85 <avaz_53@icloud.com>
2021-06-24 15:45:20 +0500

    ......

--- apelsinuz/views.py
+++ apelsinuz/views.py
@@ -1,16 +1,24 @@
+from apelsinuz.serializers import CallbackResponseSerializer
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from orders.models import OrderModel
 
-
+import json
 import logging
 
 logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def check_apelsin(request):
-    logger.warning(str(request.body))
+    data = json.load(request.body)
+    serializer = CallbackResponseSerializer(data)
+    
+    if serializer.is_valid():
+        logger.warning(str(serializer.validated_data))
+    else:
+        logger.warning(str(serializer.errors))
+
     data = {"status": False}
   
     return JsonResponse(data)
