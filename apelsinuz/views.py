5230206db1b3a1f0687221a83e8ea4f47d18e1ec
Azim85 <avaz_53@icloud.com>
2021-06-24 17:38:11 +0500

    ....

--- apelsinuz/views.py
+++ apelsinuz/views.py
@@ -18,10 +18,11 @@ def check_apelsin(request):
         data = serializer.validated_data
         if data['order_id']:
             order = OrderModel.objects.get(pk=data['order_id'])
+            logger.warning(data['amount'], data['order_id'])
+            logger.warning(order.amount, order.pk)
             if order.amount == data['amount']:
                 order.payment_status = 3
                 order.save()
-                order.refresh_from_db()
                 return JsonResponse({"status": True})
         
     return JsonResponse({"status": False})
