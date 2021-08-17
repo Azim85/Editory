636bc35e00196f94cb2f918ef4aa35b53dcab9e9
Azim85 <avaz_53@icloud.com>
2021-08-17 14:07:36 +0500

    ...

--- config/settings.py
+++ config/settings.py
@@ -225,8 +225,7 @@ LOGGING = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'azimriza@yandex.com'
-EMAIL_HOST_PASSWORD = 'applecore'
+EMAIL_HOST_PASSWORD = 'apple1976'
