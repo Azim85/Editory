3b133826ca7a2ed8dda48bf69216dec8c1795617
Azim85 <avaz_53@icloud.com>
2021-08-18 11:33:57 +0500

    ......

--- config/settings.py
+++ config/settings.py
@@ -223,9 +223,18 @@ LOGGING = {
 
 
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'rizaevazim7@gmail.com'
+# EMAIL_HOST_PASSWORD = 'apple1976'
+
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rizaevazim7@gmail.com'
-EMAIL_HOST_PASSWORD = 'apple1976'
+EMAIL_HOST_USER = 'info@editory.org'
+EMAIL_HOST_PASSWORD = 'editory.org'
